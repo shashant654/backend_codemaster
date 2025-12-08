0ce48ebfb04c5eac61816c32fe0e234924d9aa20
Shashant Shekhar <shashantshekhar10@gmail.com>
2025-12-09 01:09:34 +0530

    fif

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from fastapi import APIRouter, Depends, HTTPException, status, File, UploadFile, Form
 from sqlalchemy.orm import Session
 from app.db.database import get_db
